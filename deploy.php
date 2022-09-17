6a9c31d6229a45da17be518e6b5e7f31b0bf85d9
Robert Rupf <robert.rupf@yakamara.de>
2022-09-17 11:14:22 +0200

    add deploy.php

---
+++ deploy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Deployer;
+
+if ('cli' !== PHP_SAPI) {
+    throw new \Exception('The deployer configuration must be used in cli.');
+}
+
+// Der Pfad ist ggf. anzupassen, falls der Projekt-Root nicht dem REDAXO-Root entspricht
+require __DIR__.'/redaxo/src/addons/ydeploy/deploy.php';
+
+set('repository', 'https://github.com/xong/ydeploy_test.git');
+
+host('servername')
+    ->hostname('maumha.de')
+    ->set('deploy_path', '/kunden/536664_06108/webseiten/ydeploy-test.maumha.de')
+;
